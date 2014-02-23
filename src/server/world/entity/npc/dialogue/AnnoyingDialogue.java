d50d41c822458ad494db1d9b7969154afbf85690
lare96 <rshybrid7@hotmail.com>
2014-02-23 18:32:41 -0500

    Edited a dialogue.

--- src/server/world/entity/npc/dialogue/AnnoyingDialogue.java
+++ src/server/world/entity/npc/dialogue/AnnoyingDialogue.java
@@ -14,15 +14,15 @@ public class AnnoyingDialogue extends NpcDialogue {
     public void dialogue(Player player) {
         switch (player.getConversationStage()) {
             case 0:
-                oneLineMobDialogue(player, Expression.DISORIENTED_LEFT, "Err... hello there uh? arghh?", 956);
+                oneLineMobDialogue(player, Expression.DISORIENTED_LEFT, "Hey kid want some drugs and booze?", 956);
                 this.next(player);
                 break;
             case 1:
-                oneLineMobDialogue(player, Expression.DISORIENTED_RIGHT, "Urmph... WAIT. HAHAHA YOU'RE UGLY.", 956);
+                oneLinePlayerDialogue(player, Expression.ANNOYED, "Why would I want that?");
                 this.next(player);
                 break;
             case 2:
-                oneLinePlayerDialogue(player, Expression.ANNOYED, "Oh really? So is your mother.");
+                oneLinePlayerDialogue(player, Expression.LAUGHING, "Hahahaha pussy!");
                 this.stop(player);
                 break;
         }
