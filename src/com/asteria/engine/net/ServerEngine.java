75bd5cf5d3798a23cd1f2b5d00a2de1f00e30663
lare96 <rshybrid7@hotmail.com>
2014-08-16 17:26:34 -0400

    Cleaned up the reactor.

--- src/com/asteria/engine/net/ServerEngine.java
+++ src/com/asteria/engine/net/ServerEngine.java
@@ -11,6 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.asteria.Main;
 import com.asteria.engine.GameEngine;
 import com.asteria.engine.net.Session.Stage;
 import com.asteria.engine.net.packet.PacketDecoder;
@@ -27,18 +28,17 @@ import com.asteria.world.entity.player.Player;
  */
 public final class ServerEngine {
 
+    // TODO: Throttle packet
+
     /** A logger for printing information. */
-    private static final Logger logger = Logger.getLogger(ServerEngine.class
-            .getSimpleName());
+    private static Logger logger = Logger.getLogger(ServerEngine.class
+        .getSimpleName());
 
     /** The selector that selects keys ready to receive network events. */
     private static Selector selector;
 
     /** The server socket channel that will accept incoming connections. */
     private static ServerSocketChannel server;
-
-    /** This class cannot be instantiated. */
-    private ServerEngine() {}
 
     /**
      * Starts the core components of the reactor.
@@ -51,7 +51,7 @@ public final class ServerEngine {
         // Check if we have already started the reactor.
         if (server != null && selector != null) {
             throw new IllegalStateException(
-                    "The reactor has already been started!");
+                "The reactor has already been started!");
         }
 
         // Create the networking objects.
@@ -93,8 +93,197 @@ public final class ServerEngine {
             // Unable to restart, so print the exception and shutdown the
             // server.
             logger.log(Level.SEVERE,
-                    "Unable to restart the reactor, shutting down!", e);
+                "Unable to restart the reactor, shutting down!", e);
             World.shutdown();
+        }
+    }
+
+    /**
+     * Submits a task to the {@link GameEngine} that will asynchronously accept
+     * all incoming connections for this cycle. If after <tt>5</tt> attempts no
+     * connections have been made, the task returns.
+     */
+    private static void acceptClients() {
+
+        // Accept the key asynchronously if needed.
+        GameEngine.getServiceExecutor().execute(new Runnable() {
+
+            /** A thread safe integer for holding the times looped. */
+            private final AtomicInteger loopsMade = new AtomicInteger();
+
+            @Override
+            public void run() {
+                SocketChannel socket;
+
+                try {
+
+                    // Accept the connection.
+                    while ((socket = server.accept()) != null || loopsMade
+                        .get() <= 5) {
+
+                        // Increment the loop count.
+                        loopsMade.incrementAndGet();
+
+                        // Check if the connection is valid.
+                        if (socket == null) {
+                            continue;
+                        }
+
+                        // Block if we fail the security check.
+                        if (!HostGateway.enter(socket.socket().getInetAddress()
+                            .getHostAddress())) {
+                            socket.close();
+                            continue;
+                        }
+
+                        // Otherwise create a new session.
+                        socket.configureBlocking(false);
+                        SelectionKey newKey = socket.register(selector,
+                            SelectionKey.OP_READ);
+                        newKey.attach(new Session(newKey));
+                    }
+                } catch (IOException e) {
+                    logger.log(Level.WARNING,
+                        "Error while accepting incoming clients!", e);
+                }
+            }
+        });
+    }
+
+    /**
+     * Decodes and handles all incoming packets sent from the client for this
+     * cycle.
+     * 
+     * @param key
+     *            the selection key that holds the session instance.
+     */
+    private static void decodePackets(SelectionKey key) {
+
+        // Grab the session attachment from the key.
+        Session session = (Session) key.attachment();
+
+        if (session == null) {
+            return;
+        }
+
+        try {
+
+            // Decode packets for the key, and handle login if needed.
+            if (session.getSocketChannel().read(session.getInData()) == -1) {
+                session.disconnect();
+                return;
+            }
+
+            session.getInData().flip();
+
+            while (session.getInData().hasRemaining()) {
+
+                // There's data to be read, reset the timeout.
+                session.getTimeout().reset();
+
+                // Handle login here if needed.
+                if (session.getStage() != Stage.LOGGED_IN) {
+                    session.handleLogin();
+                    break;
+                }
+
+                // Decode the packet opcode and packet length.
+                if (session.getPacketOpcode() == -1) {
+                    session.setPacketOpcode(session.getInData().get() & 0xff);
+                    session.setPacketOpcode(session.getPacketOpcode() - session
+                        .getDecryptor().getKey() & 0xff);
+                }
+
+                if (session.getPacketLength() == -1) {
+                    session.setPacketLength(Utility.PACKET_LENGTHS[session
+                        .getPacketOpcode()]);
+
+                    if (session.getPacketLength() == -1) {
+                        if (!session.getInData().hasRemaining()) {
+                            session.getInData().flip();
+                            session.getInData().compact();
+                            break;
+                        }
+
+                        session
+                            .setPacketLength(session.getInData().get() & 0xff);
+                    }
+                }
+
+                // Handle the decoded packet and make sure all of the
+                // data is read
+                if (session.getInData().remaining() >= session
+                    .getPacketLength()) {
+                    int positionBefore = session.getInData().position();
+
+                    try {
+                        if (PacketDecoder.getPackets()[session
+                            .getPacketOpcode()] != null) {
+                            PacketDecoder.getPackets()[session
+                                .getPacketOpcode()].decode(session.getPlayer(),
+                                new ProtocolBuffer(session.getInData()));
+                        } else {
+                            if (Main.DEBUG)
+                                logger
+                                    .info(session.getPlayer() + " unhandled packet " + session
+                                        .getPacketOpcode());
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    } finally {
+                        int read = session.getInData().position() - positionBefore;
+
+                        for (int i = read; i < session.getPacketLength(); i++) {
+                            session.getInData().get();
+                        }
+                    }
+                    session.setPacketOpcode(-1);
+                    session.setPacketLength(-1);
+                } else {
+                    session.getInData().flip();
+                    session.getInData().compact();
+                    break;
+                }
+            }
+
+            // Clear the buffer for the next cycle.
+            session.getInData().clear();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.disconnect();
+        }
+    }
+
+    /**
+     * Sends any previously queued data in the session's <code>outData</code>
+     * buffer to its socket channel.
+     * 
+     * @param key
+     *            the selection key that holds the session instance.
+     */
+    private static void sendQueuedData(SelectionKey key) {
+
+        // Grab the session attachment from the key.
+        Session session = (Session) key.attachment();
+
+        if (session == null) {
+            return;
+        }
+
+        try {
+
+            // Send any previously queued data if needed.
+            session.getOutData().flip();
+            session.getSocketChannel().write(session.getOutData());
+
+            if (!session.getOutData().hasRemaining()) {
+                session.getOutData().clear();
+            } else {
+                session.getOutData().compact();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            session.disconnect();
         }
     }
 
@@ -111,7 +300,7 @@ public final class ServerEngine {
             selector.selectNow();
         } catch (IOException io) {
             logger.log(Level.WARNING,
-                    "Error during event selection, restarting the reactor!", io);
+                "Error during event selection, restarting the reactor!", io);
 
             // Something happened while selecting, attempt to restart!
             restart();
@@ -119,179 +308,27 @@ public final class ServerEngine {
         }
 
         // Handle all of the selected events.
-        for (final Iterator<SelectionKey> iterator = getSelector()
-                .selectedKeys().iterator(); iterator.hasNext();) {
+        for (final Iterator<SelectionKey> iterator = selector.selectedKeys()
+            .iterator(); iterator.hasNext();) {
             SelectionKey key = iterator.next();
 
             if (!key.isValid()) {
-
-                // Remove the key if its invalid.
                 iterator.remove();
-
             } else if (key.isAcceptable()) {
-
-                // Accept the key asynchronously if needed.
                 try {
-                    GameEngine.getServiceExecutor().execute(new Runnable() {
-
-                        /** A thread safe integer for holding the times looped. */
-                        private final AtomicInteger loopsMade = new AtomicInteger();
-
-                        @Override
-                        public void run() {
-                            SocketChannel socket;
-
-                            try {
-
-                                // Accept the connection.
-                                while ((socket = ServerEngine.getServer()
-                                        .accept()) != null || loopsMade.get() <= 5) {
-
-                                    // Increment the loop count.
-                                    loopsMade.incrementAndGet();
-
-                                    // Check if the connection is valid.
-                                    if (socket == null) {
-                                        continue;
-                                    }
-
-                                    // Block if we fail the security check.
-                                    if (!HostGateway.enter(socket.socket()
-                                            .getInetAddress().getHostAddress())) {
-                                        socket.close();
-                                        continue;
-                                    }
-
-                                    // Otherwise create a new session.
-                                    socket.configureBlocking(false);
-                                    SelectionKey newKey = socket.register(
-                                            ServerEngine.getSelector(),
-                                            SelectionKey.OP_READ);
-                                    newKey.attach(new Session(newKey));
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+                    acceptClients();
                 } finally {
                     iterator.remove();
                 }
-
             } else if (key.isReadable()) {
-
-                // Decode packets for the key, and handle login if needed.
-                Session session = (Session) key.attachment();
-
-                if (session == null) {
-                    continue;
-                }
-
                 try {
-                    if (session.getSocketChannel().read(session.getInData()) == -1) {
-                        session.disconnect();
-                        continue;
-                    }
-
-                    session.getInData().flip();
-
-                    while (session.getInData().hasRemaining()) {
-
-                        // There's data to be read, reset the timeout.
-                        session.getTimeout().reset();
-
-                        // Handle login here if needed.
-                        if (session.getStage() != Stage.LOGGED_IN) {
-                            session.handleLogin();
-                            break;
-                        }
-
-                        // Decode the packet opcode and packet length.
-                        if (session.getPacketOpcode() == -1) {
-                            session.setPacketOpcode(session.getInData().get() & 0xff);
-                            session.setPacketOpcode(session.getPacketOpcode() - session
-                                    .getDecryptor().getKey() & 0xff);
-                        }
-
-                        if (session.getPacketLength() == -1) {
-                            session.setPacketLength(Utility.PACKET_LENGTHS[session
-                                    .getPacketOpcode()]);
-
-                            if (session.getPacketLength() == -1) {
-                                if (!session.getInData().hasRemaining()) {
-                                    session.getInData().flip();
-                                    session.getInData().compact();
-                                    break;
-                                }
-
-                                session.setPacketLength(session.getInData()
-                                        .get() & 0xff);
-                            }
-                        }
-
-                        // Handle the decoded packet and make sure all of the
-                        // data is read
-                        if (session.getInData().remaining() >= session
-                                .getPacketLength()) {
-                            int positionBefore = session.getInData().position();
-
-                            try {
-                                if (PacketDecoder.getPackets()[session
-                                        .getPacketOpcode()] != null) {
-                                    PacketDecoder.getPackets()[session
-                                            .getPacketOpcode()].decode(session
-                                            .getPlayer(), new ProtocolBuffer(
-                                            session.getInData()));
-                                } else {
-                                    // logger.info(session.getPlayer()
-                                    // + " unhandled packet "
-                                    // + session.getPacketOpcode());
-                                }
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
-                            } finally {
-                                int read = session.getInData().position() - positionBefore;
-
-                                for (int i = read; i < session
-                                        .getPacketLength(); i++) {
-                                    session.getInData().get();
-                                }
-                            }
-                            session.setPacketOpcode(-1);
-                            session.setPacketLength(-1);
-                        } else {
-                            session.getInData().flip();
-                            session.getInData().compact();
-                            break;
-                        }
-                    }
-
-                    // Clear the buffer for the next cycle.
-                    session.getInData().clear();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    session.disconnect();
+                    decodePackets(key);
                 } finally {
                     iterator.remove();
                 }
-
             } else if (key.isWritable()) {
-
-                // Send any previously queued data if needed.
-                Session session = (Session) key.attachment();
-
                 try {
-                    session.getOutData().flip();
-                    session.getSocketChannel().write(session.getOutData());
-
-                    if (!session.getOutData().hasRemaining()) {
-                        session.getOutData().clear();
-                    } else {
-                        session.getOutData().compact();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    session.disconnect();
+                    sendQueuedData(key);
                 } finally {
                     iterator.remove();
                 }
@@ -299,21 +336,5 @@ public final class ServerEngine {
         }
     }
 
-    /**
-     * Gets the selector instance.
-     * 
-     * @return the selector.
-     */
-    public static Selector getSelector() {
-        return ServerEngine.selector;
-    }
-
-    /**
-     * Gets the server socket channel instance.
-     * 
-     * @return the server socket channel.
-     */
-    public static ServerSocketChannel getServer() {
-        return ServerEngine.server;
-    }
+    private ServerEngine() {}
 }
