c0928568ca1534f4f95d2a8a20a4999186e3bdc5
lare96 <rshybrid7@hotmail.com>
2014-08-16 15:28:54 -0400

    Updated the run.bat to work with my java version.

--- asteria_run.bat
+++ asteria_run.bat
@@ -1,4 +1,4 @@
 @echo off
 title asteria_run
-"C:/Program Files/Java/jdk1.7.0_51/bin/java.exe" -Xmx1024m -cp bin;libs/gson-2.2.4.jar; com.asteria.Main 
+"C:/Program Files/Java/jdk1.8.0_11/bin/java.exe" -Xmx1024m -cp bin;libs/gson-2.2.4.jar; com.asteria.Main 
 pause
